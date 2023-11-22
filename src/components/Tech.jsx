b970fa0d5c060b994d22acc73e9d6bd11d31caba
gemaquejr <gemaquejr@hotmail.com>
2023-11-22 14:33:17 -0300

    Remove 3d das tecnologias
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+// import React from "react";
 
-import { BallCanvas } from "./canvas";
-import { SectionWrapper } from "../hoc";
+// import { BallCanvas } from "./canvas";
+// import { SectionWrapper } from "../hoc";
+// import { technologies } from "../constants";
+
+// const Tech = () => {
+//   return (
+//     <div className='flex flex-row flex-wrap justify-center gap-10'>
+//       {technologies.map((technology) => (
+//         <div className='w-28 h-28' key={technology.name}>
+//           <BallCanvas icon={technology.icon} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default SectionWrapper(Tech, "");
+
+import React from "react";
+import { motion } from "framer-motion";
+
 import { technologies } from "../constants";
+import { styles } from "../styles";
+import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+  <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Tecnologias</p>
+      <h2 className={styles.sectionHeadText}>Full stack.</h2>
+    </motion.div>
+
+    <div className='mt-12 flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <img src={technology.icon} alt={technology.name} className='w-16 h-16 object-contain' />
         </div>
       ))}
     </div>
+  </>
   );
 };
 
-export default SectionWrapper(Tech, "");
+export default Tech;
